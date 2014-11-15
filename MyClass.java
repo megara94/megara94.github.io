f5f6bf79960cb09aef244b5bea716cbacba7a309
Linda Avendaño Franco <megara94@yahoo.com>
2014-11-15 15:47:51 -0800

    Continue with the example

--- MyClass.java
+++ MyClass.java
@@ -1,2 +1,10 @@
 public class MyClass{
+  
+  public String toString(){
+    return "MyClass is awesome";
+  }
+  
+  public static void main(String args[]){
+    System.out.println(new MyClass().toString());
+  }
 }
